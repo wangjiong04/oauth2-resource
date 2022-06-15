00ae978a677b4af6d9493a988fc209127f12acb9
wangjiong04 <44149679+wangjiong04@users.noreply.github.com>
2022-06-15 21:10:15 +0800

    Add files via upload

---
+++ src/test/java/com/example/oauth2/resource/ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
