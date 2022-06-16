724fc43af688fa2f9f0d316d0d4813f0150d2baa
Jonathan Wang <jonathan.wang01@sap.com>
2022-06-16 22:54:56 +0800

    add token converter

---
+++ src/main/java/com/example/oauth2/resource/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.oauth2.resource.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
